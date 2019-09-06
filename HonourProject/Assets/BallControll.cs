adc0badd3fb4be2471aba11aa474f541cac9de8e
IceRain <664073296@qq.com>
2019-09-06 14:12:23 -0400

    Proof of concept - read from animation
    Successfully read the animation data from unity animator.

---
+++ HonourProject/Assets/BallControll.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class BallControll : MonoBehaviour
+{
+    public AnimationClip BallAnimation;
+    public Text output;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EditorCurveBinding[] curveBindings;
+        AnimationCurve[] curves;
+        curveBindings = UnityEditor.AnimationUtility.GetCurveBindings(BallAnimation);
+        curves = new AnimationCurve[curveBindings.Length];
+        string outputtext = "";
+        for (int i = 0; i < curves.Length; i++)
+        {
+            curves[i] = AnimationUtility.GetEditorCurve(BallAnimation, curveBindings[i]);
+            Keyframe[] keyframes = curves[i].keys;
+            string s = "";
+            if (keyframes.Length > 0)
+            {
+                outputtext += "Keyframes in curve " + i + " : ";
+                for (int a = 0; a < keyframes.Length; a++)
+                {
+                    s += keyframes[a].value + ", ";
+                }
+                outputtext+=s+"\n";
+            }
+            else
+            {
+            }
+        }
+        output.text = outputtext;
+    }
+}
